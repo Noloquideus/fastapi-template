d4a3e98d2d07994f19740f3a364a01eafbd99924
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 16:28:14 +0300

    feat: add base incomparable object

---
+++ src/application/domain/objects/incomparable.py
@@ -0,0 +1,39 @@
+from abc import ABC
+from src.application.domain.exceptions import IncomparableObjectError
+
+
+class Incomparable(ABC):
+    """
+    Base class for objects that cannot be compared.
+
+    Any attempt to perform comparison or hashing operations will raise an exception.
+    This is useful for objects where comparison logic does not make sense.
+    """
+
+    def __eq__(self, other):
+        """Raises an exception when trying to check equality."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __ne__(self, other):
+        """Raises an exception when trying to check inequality."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __lt__(self, other):
+        """Raises an exception when trying to check if less than."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __le__(self, other):
+        """Raises an exception when trying to check if less than or equal."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __gt__(self, other):
+        """Raises an exception when trying to check if greater than."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __ge__(self, other):
+        """Raises an exception when trying to check if greater than or equal."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
+
+    def __hash__(self):
+        """Raises an exception when trying to hash the object."""
+        raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be hashed')
