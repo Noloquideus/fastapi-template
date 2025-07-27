1099cf3916ea3f781eed0fd708767b78ba2bc1ad
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 16:34:24 +0300

    feat: add handler for exception sealed object

---
+++ src/presentation/handlers/sealed_class_error_handler.py
@@ -0,0 +1,10 @@
+from fastapi.responses import JSONResponse
+from src.application.domain.enums.status_code import StatusCode
+from src.application.domain.exceptions import SealedClassError
+
+
+async def sealed_class_error_handler(exc: SealedClassError) -> JSONResponse:
+    return JSONResponse(
+        status_code=StatusCode.INTERNAL_SERVER_ERROR.value,
+        content={'error': 'Error in data types', 'detail': str(exc)}
+    )
