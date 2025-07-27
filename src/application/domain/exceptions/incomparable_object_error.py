8b61e38d12d1ca12603bec63db1f794dfbee84da
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 16:29:36 +0300

    feat: add exception for incomparable object

---
+++ src/application/domain/exceptions/incomparable_object_error.py
@@ -0,0 +1,21 @@
+from src.application.domain.enums.status_code import StatusCode
+
+
+class IncomparableObjectError(TypeError):
+    """Raised when attempting to inherit from a sealed class."""
+    __slots__ = ['_status_code', '_message']
+
+    def __init__(self, status_code: StatusCode, message: str):
+        self._status_code = status_code
+        self._message = message
+
+    @property
+    def status_code(self) -> StatusCode:
+        return self._status_code
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    def __str__(self) -> str:
+        return f'{self.status_code.value} - {self.message}'
