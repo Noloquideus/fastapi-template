04a13844f90f7504480ad5bd0e00a731b598ed0d
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 16:25:16 +0300

    feat: add base exception

---
+++ src/application/domain/exceptions/base.py
@@ -0,0 +1,23 @@
+from src.application.domain.enums.status_code import StatusCode
+from src.application.domain.objects.immutable import Immutable
+from src.application.domain.objects.incomparable import Incomparable
+
+
+class ApplicationException(Exception, Incomparable, Immutable):
+    """Base class for application exceptions"""
+    __slots__ = ['_status_code', '_message']
+
+    def __init__(self, status_code: StatusCode, message: str):
+        self._status_code = status_code
+        self._message = message
+
+    @property
+    def status_code(self) -> StatusCode:
+        return self._status_code
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    def __str__(self) -> str:
+        return f'{self.status_code.value} - {self.message}'
