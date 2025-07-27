133edb25ecf80b048f1e1d6bb9a8eab9b915f1e0
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 17:08:54 +0300

    fix: add typing

--- src/presentation/middleware/timing.py
+++ src/presentation/middleware/timing.py
@@ -1,15 +1,17 @@
 import time
+from typing import Callable, Awaitable
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.applications import Starlette
 from fastapi import Request, Response
 from src.infrastructure.logger import Logger
 
 
 class TimingMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, logger: Logger):
+    def __init__(self, app: Starlette, logger: Logger) -> None:
         super().__init__(app)
         self.logger = logger
 
-    async def dispatch(self, request: Request, call_next) -> Response:
+    async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
         start_time = time.perf_counter()
         response = await call_next(request)
         duration = (time.perf_counter() - start_time) * 1000  # milliseconds
