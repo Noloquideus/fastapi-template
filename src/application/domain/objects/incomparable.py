580556ba5750db9b476b564f1e9b45308d4247e0
Noloquideus <daniilmanukian@gmail.com>
2025-07-27 17:06:45 +0300

    fix: add typing

--- src/application/domain/objects/incomparable.py
+++ src/application/domain/objects/incomparable.py
@@ -10,30 +10,30 @@ class Incomparable(ABC):
     This is useful for objects where comparison logic does not make sense.
     """
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Raises an exception when trying to check equality."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         """Raises an exception when trying to check inequality."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __lt__(self, other):
+    def __lt__(self, other: object) -> bool:
         """Raises an exception when trying to check if less than."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __le__(self, other):
+    def __le__(self, other: object) -> bool:
         """Raises an exception when trying to check if less than or equal."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __gt__(self, other):
+    def __gt__(self, other: object) -> bool:
         """Raises an exception when trying to check if greater than."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __ge__(self, other):
+    def __ge__(self, other: object) -> bool:
         """Raises an exception when trying to check if greater than or equal."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be compared')
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         """Raises an exception when trying to hash the object."""
         raise IncomparableObjectError(f'{self.__class__.__name__} objects cannot be hashed')
